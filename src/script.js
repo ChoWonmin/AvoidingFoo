ba3dd77eeb5e05514b1562649f912a6861df2bdd
ChoWonmin <0108257@gmail.com>
2020-03-20 21:54:04 +0900

    cotroller js

--- src/script.js
+++ src/script.js
@@ -13,6 +13,11 @@ const player = new Player();
 let bombs = [];
 bombs.push(new Bomb());
 
+// 0: stop
+// 1: left
+// 2:right
+let controller = 0;
+
 function setup() {
   createCanvas(width, height).parent("content");
   mapImage.preLoad("ice.jpeg");
@@ -23,10 +28,15 @@ function setup() {
 function update() {
   if (keyIsPressed === false) player.stop();
 
-  if (keyIsDown(LEFT_ARROW)) player.moveLeft();
-  else if (keyIsDown(RIGHT_ARROW)) player.moveRight();
+  console.log(controller);
 
-  if (player.status === "live") {
+  if (keyIsDown(LEFT_ARROW) || controller === 1) {
+    player.moveLeft();
+  } else if (keyIsDown(RIGHT_ARROW) || controller === 2) {
+    player.moveRight();
+  }
+
+  if (player.status !== "die") {
     score++;
   }
 
@@ -39,13 +49,14 @@ function update() {
 
 function draw() {
   update();
+
   background(255, 255, 255);
   mapImage.drawAll();
-  fill(255);
+
   tick++;
 
   if (tick % 50 === parseInt(Math.random() * 50)) {
-    if (bombs.length < 20) bombs.push(new Bomb());
+    if (bombs.length < 3) bombs.push(new Bomb());
   }
 
   for (let i = 0; i < bombs.length; i++) {
@@ -53,7 +64,6 @@ function draw() {
     bomb.draw();
   }
 
-  if (!player.live) fill(255, 0, 0);
   player.draw();
 }
 
@@ -67,9 +77,19 @@ restartBtn.addEventListener("click", () => {
 
 const leftBtn = document.getElementById("left-btn");
 leftBtn.addEventListener("click", () => {
-  console.log("left");
+  controller = 1;
+});
+
+const rightBtn = document.getElementById("right-btn");
+rightBtn.addEventListener("click", () => {
+  controller = 2;
+});
+
+const stopBtn = document.getElementById("stop-btn");
+stopBtn.addEventListener("click", () => {
+  controller = 0;
 });
 
 setInterval(() => {
   document.getElementsByClassName("score")[0].innerHTML = score;
-}, 500);
+}, 1000);
